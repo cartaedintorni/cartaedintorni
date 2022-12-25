5477481af24f9caac3b811a43f11cbe10b43ca3e
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-12-25 04:05:10 +0100

    prod

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,61 +1,39 @@
-var avsc = Array.from(document.getElementsByTagName("script")).map(x=>x.src)
-var need= [
-    {src: "https://apis.google.com/js/api.js", onLoad: ()=>{
+var genurl =function (mode,foglio) {return "https://sheets.googleapis.com/v4/spreadsheets/1opQsMabvYY9XBNyoX4jLiIz2VFDaVJ-6zYPruKAKNF4/"+mode+"/"+foglio+"?key=AIzaSyCqdr8_RYbGV1pkfrZ81blCKD8eNWWO9GA"}
+var url = genurl("values","Prodotti")
 
-    }},
-    {src: "https://accounts.google.com/gsi/client", onLoad: ()=>{
-        readProduct()
-    }}
-]
-need.forEach(s=>{
-    if (!avsc.includes(s.src)) {
-        var src=document.createElement("script")
-        src.src=s.src
-        src.onload=s.onLoad
-        src.defer=true
-        src.async=true
-        document.body.appendChild(src)
+var fetchAll = function (callback) {$.get(url,(a)=>{
+    if (callback) callback(a)
+})}
+
+class Product {
+    constructor(name, type, desc, img, price) {
+        this.name=name
+        this.type=type
+        this.desc=desc
+        this.img=img
+        this.price=price
     }
-})
 
-var docs = {
-    doc: {
-        id: "1opQsMabvYY9XBNyoX4jLiIz2VFDaVJ-6zYPruKAKNF4",
-        type: "spreadsheets",
-        baseurl: "https://docs.google.com",
-        url: "https://docs.google.com/spreadsheets/d/1opQsMabvYY9XBNyoX4jLiIz2VFDaVJ-6zYPruKAKNF4"
-    },
-    data: {
-        key: "AIzaSyCqdr8_RYbGV1pkfrZ81blCKD8eNWWO9GA",
-        CLIENT_ID: '110546839858020135233',
-        API_KEY: '755fdd29af566db9ecdf3b4988509f2733a048a4',
-        SCOPES: ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    }
-}
-
-let tokenClient;
-let gapiInited = false;
-let gisInited = false;
-gapi.load("client",readProduct)
-async function readProduct() {
-    if (!gapi.client) {
-        await gapi.client.init({
-            apiKey: docs.data.API_KEY,
-            discoveryDocs: docs.data.SCOPES
+    static fromArrays(ia) {
+        var arr = Array.from(ia)
+        var x = arr.shift().map(x=>x.toLowerCase())
+        var i = {
+            name: x.indexOf("nome"),
+            type: x.indexOf("marca"),
+            desc: x.indexOf("descrizione"),
+            img: x.indexOf("foto"),
+            price: x.indexOf("prezzo")
+        }
+        var prods = []
+        arr.forEach(function (ap) {
+            if (ap.length!=x.length) {return}
+            prods.push(new Product(
+                ap[i.name],
+                ap[i.type],
+                ap[i.desc],
+                ap[i.img],
+                ap[i.price]
+            ))
         })
-    }
-    try {
-        gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: docs.doc.id,
-            range: "Prodotti",
-        }).then((response) => {
-            const result = response.result;
-            const numRows = result.values ? result.values.length : 0;
-            console.log(`${numRows} rows retrieved.`);
-            if (callback) callback(response);
-        });
-    } catch (error) {
-        console.log(error)
-        return;
     }
 }
